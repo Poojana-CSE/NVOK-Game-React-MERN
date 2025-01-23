ec933768736043d3c24a7b98b2186856b47524dd
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-23 11:06:21 +0530

    Minimally Done

--- src/App.js
+++ src/App.js
@@ -7,11 +7,32 @@ const App = () => {
   const [currentquestion, setcurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [selectOption, setSelectOption] = useState(null);
+  const [showScore,setShowScore] = useState(false);
   const [timer,setTimer] = useState(10);
 
   useEffect(()=>{
+    let interval;
+    if(timer>0 && !showScore){
+      interval = setInterval(()=>{
+        setTimer((prevTime)=>prevTime-1);
+      },1000);
+    }
+    else if(timer===0){
+        if(currentquestion < data.length - 1){
+          setcurrentQuestion((preQues)=>preQues+1);
+          setTimer(10);
+          setSelectOption(null);
+        }
+        else{
+          setShowScore(true);
+        }
+      
+    }
     
-  })
+    return ()=>clearInterval(interval);
+  },[timer,showScore,currentquestion]);
+
+
 
   const handleClick = (option) => {
     setSelectOption(option);
@@ -26,19 +47,26 @@ const App = () => {
     }
   };
 
-
+  function restartQuiz(){
+    setcurrentQuestion(0);
+    setTimer(10);
+    setShowScore(false);
+    setScore(0);
+    setSelectOption(null);
+  }
   return (
     <div className='quiz-app'>
-      <div className='score-section'>
+      {showScore ? (      
+        <div className='score-section'>
         <h3>Score: {score}/{data.length}</h3>
-        <button>Restart</button>
+        <button onClick={restartQuiz}>Restart</button>
       </div>
-      <div className='question-section'>
+    ):(
+    <div className='question-section'>
         <h2></h2>
         <p>{data[currentquestion].question}</p>
         <div className='options'>
-          {data[currentquestion].options.map((option, index) => {
-            return (
+          {data[currentquestion].options.map((option, index) => (
               <button
                 key={index}
                 onClick={() => handleClick(option)}
@@ -46,13 +74,18 @@ const App = () => {
                 }}>
                 {option}
               </button>
-            );
-          })}
+            )
+          )}
         </div>
-        <div className='timer'>Time Left: <span>10s</span></div>
+        <div className='timer'>Time Left: <span>{timer}</span></div>
       </div>
+    )}
     </div>
   );
 };
 
 export default App;
+
+// };
+
+// export default App;
