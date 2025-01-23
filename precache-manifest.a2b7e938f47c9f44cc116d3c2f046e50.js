983b0f84b57e239c7552eca50d70faf4a47c2d44
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-23 11:39:26 +0530

    Updates

---
+++ precache-manifest.a2b7e938f47c9f44cc116d3c2f046e50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adddbb75e78ea0b6a2b76fb60eae8a80",
+    "url": "/NVOK-Game-React-MERN/index.html"
+  },
+  {
+    "revision": "e693d7063f4c2ee584a4",
+    "url": "/NVOK-Game-React-MERN/static/css/main.639757a4.chunk.css"
+  },
+  {
+    "revision": "88ace857bc0b42e99152",
+    "url": "/NVOK-Game-React-MERN/static/js/2.b836a3fe.chunk.js"
+  },
+  {
+    "revision": "e693d7063f4c2ee584a4",
+    "url": "/NVOK-Game-React-MERN/static/js/main.45d61a25.chunk.js"
+  },
+  {
+    "revision": "4edc66c023eb368fe035",
+    "url": "/NVOK-Game-React-MERN/static/js/runtime~main.b12372bf.js"
+  },
+  {
+    "revision": "889b8ea9616d69c19dbd3754aea454e5",
+    "url": "/NVOK-Game-React-MERN/static/media/bg.889b8ea9.jpg"
+  },
+  {
+    "revision": "d27f45adf88f4e70e787423e4db695ad",
+    "url": "/NVOK-Game-React-MERN/static/media/correct.d27f45ad.mp3"
+  },
+  {
+    "revision": "e55862ad3acb9ac8ac4454221366f389",
+    "url": "/NVOK-Game-React-MERN/static/media/wrong.e55862ad.mp3"
+  }
+]);
