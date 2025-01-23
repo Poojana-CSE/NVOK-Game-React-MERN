57ab93c69db79527b89e1868dfd514d650c2ed5a
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-23 09:57:45 +0530

    Audio Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import data from './questions.json';
+import correct from './sounds/correct.mp3'
+import wrong from './sounds/wrong.mp3'
 const App = () => {
-  const [currentquestion,setcurrentQuestion] = useState(0);
-  const [score,setScore] = useState(0);
+  const [currentquestion, setcurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [selectOption, setSelectOption] = useState(null);
+  const [timer,setTimer] = useState(10);
+
+  useEffect(()=>{
+    
+  })
+
+  const handleClick = (option) => {
+    setSelectOption(option);
+    if(option===data[currentquestion].correctOption){
+      setScore((val)=>val+1);
+      const audio = new Audio(correct);
+      audio.play();
+    }
+    else{
+      const audio = new Audio(wrong);
+      audio.play();
+    }
+  };
 
 
   return (
-    <div>
+    <div className='quiz-app'>
+      <div className='score-section'>
+        <h3>Score: {score}/{data.length}</h3>
+        <button>Restart</button>
+      </div>
       <div className='question-section'>
-        <h2>This is a sample question?</h2>
+        <h2></h2>
+        <p>{data[currentquestion].question}</p>
         <div className='options'>
-          <button>Option 1</button>
-          <button>Option 2</button>
-          <button>Option 3</button>
-          <button>Option 4</button>
+          {data[currentquestion].options.map((option, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => handleClick(option)}
+                style={{ backgroundColor:selectOption === option ? option === data[currentquestion].correctOption ? 'green' : 'red': '',
+                }}>
+                {option}
+              </button>
+            );
+          })}
         </div>
         <div className='timer'>Time Left: <span>10s</span></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
